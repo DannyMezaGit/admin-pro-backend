ed15454bafa0c8e338bf05c68725537e983d104f
Daniel Meza <danielmeza.4c@gmail.com>
2021-04-24 23:09:27 -0700

    validación de campos obligatorios en usuarios - email - passsword - nombre

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,6 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
+
 const Usuario = require('../models/usuario');
 
 const getUsuarios = async (req, res) => {
@@ -13,6 +15,17 @@ const getUsuarios = async (req, res) => {
 const crearUsuario = async (req, res = response) => {
 
     const {email, password, nombre} = req.body;
+
+    const errores = validationResult(req);
+
+    if (!errores.isEmpty()) {
+
+      return res.status(400).json({
+        ok: false,
+        errors: errores.mapped()
+      })
+      
+    }
 
     try {
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,11 +2,21 @@
     Ruta: /api/usuarios
 */
 const {Router} = require("express");
+const {check} = require("express-validator");
+
 const { getUsuarios, crearUsuario } = require('../controllers/usuarios');
 
 const router = Router();
 
+
 router.get('/', getUsuarios);
-router.post('/', crearUsuario);
+
+router.post('/', [
+
+    check('nombre', 'el nombre es obligatorio').not().isEmpty(),
+    check('password', 'password obligatorio').not().isEmpty(),
+    check('email', 'email obligatorio').isEmail()
+
+], crearUsuario);
 
 module.exports = router;
